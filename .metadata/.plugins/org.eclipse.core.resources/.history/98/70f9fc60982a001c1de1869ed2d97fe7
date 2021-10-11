850f7da461b0e9fd95c17587b12d38a2831363d0
amit-tech01 <amit.kumar@ecerasystem.com>
2021-10-11 19:25:34 +0530

    matrix rotate by 90 degree

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/98/70f9fc60982a001c1de1869ed2d97fe7
@@ -0,0 +1,9 @@
+
+public class matrix_rotate_90_degree_right {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Codes_1/src/matrix_rotate_90_degree_right.java
+++ Codes_1/src/matrix_rotate_90_degree_right.java
@@ -1,8 +1,49 @@
-
+import java.util.*;
 public class matrix_rotate_90_degree_right {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int m= sc.nextInt();
+		int arr[][] = new int[n][m];
+		for(int i =0;i<arr.length;i++) {
+			for(int j=0;j<arr[0].length;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		
+	    // transpose
+	    for (int i = 0; i < arr.length; i++) {
+	      for (int j = 0; j < i; j++) {
+	        int temp = arr[i][j];
+	        arr[i][j] = arr[j][i];
+	        arr[j][i] = temp;
+	      }
+	    }
+	    
+	    // reverse columns, row by row
+	    for (int i = 0; i < arr.length; i++) {
+	      int li = 0;
+	      int ri = arr[0].length - 1;
+	      while (li <= ri) {
+	        int temp = arr[i][li];
+	        arr[i][li] = arr[i][ri];
+	        arr[i][ri] = temp;
+
+	        li++;
+	        ri--;
+	      }
+	    }
+		
+		//printing the output array
+		for(int i =0;i<arr.length;i++) {
+			for(int j=0;j<arr[0].length;j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+		
 
 	}
 
