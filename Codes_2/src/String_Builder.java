0ea575b7f1b767f481edc25b9f3b5531a99b4e70
amit-tech01 <amit.kumar@ecerasystem.com>
2021-10-16 17:09:45 +0530

    Added String builder

---
+++ Codes_2/src/String_Builder.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class String_Builder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    StringBuilder sb = new StringBuilder("Hello");
+	    System.out.println("String : " + sb);
+	    // 1.
+	    System.out.println("Length of string : " + sb.length());
+	    // 2.
+	    System.out.println("First character : " + sb.charAt(0));
+	    // 3.
+	    sb.setCharAt(0, 'h');
+	    System.out.println("String : " + sb);
+	    // 4.
+	    sb.append(" reader");
+	    System.out.println("String : " + sb);
+	    //5.
+	    sb.deleteCharAt(5);
+	    System.out.println("String : " + sb);
+	    // 6.
+	    sb.delete(0, 5);
+	    System.out.println("String : " + sb);
+	    // 7.
+	    sb.replace(4, 6, "ing");
+	    System.out.println("String : " + sb);
+	    // 8.
+	    String str = sb.toString();
+	    System.out.println("String : " + str);
+	}
+
+}
