8489fdefa53f28fada200e8f1dd8be22c4e3eefb
amit-tech01 <amit.kumar@ecerasystem.com>
2021-10-23 22:20:47 +0530

    added all indices code in recursion

--- Recursion_&_Backtracking_1/src/Array_Print_All_Indices.java
+++ Recursion_&_Backtracking_1/src/Array_Print_All_Indices.java
@@ -26,20 +26,18 @@ public class Array_Print_All_Indices {
 
     public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
         // write ur code here
-        if(idx==arr.length) {
-        	return -1;
-        }
-        int liisa = lastIndex(arr,idx+1,x); //last index in smaller array
-       
-        if(liisa==-1) {
-        	if(arr[idx]==x) {
-        		return idx;
-        	} else {
-        		return -1;
-        	}
-        } else {
-        	return liisa;
-        }
+    	if(idx==arr.length) {
+    		return new int[fsf];
+    	}
+    	
+    	if(arr[idx]==x) {
+    		int[] iarr = allIndices(arr,x,idx+1,fsf+1);
+    		iarr[fsf] = idx;
+    		return iarr;
+    	} else {
+    		int[] iarr = allIndices(arr,x,idx+1,fsf);
+    		return iarr;
+    	}
     	
     }
 }
