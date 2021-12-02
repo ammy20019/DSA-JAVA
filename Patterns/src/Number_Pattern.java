8f4f67cdbf178723bdf692b62cff4572d6151e85
amit-tech01 <amit.kumar@ecerasystem.com>
2021-12-02 21:42:40 +0530

    patterns pasca;, triangle char, num triangle

---
+++ Patterns/src/Number_Pattern.java
@@ -0,0 +1,15 @@
+/* floyd's triangle */
+public class Number_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=1;
+		for(int i=1;i<5;i++) {
+			for(int j=1;j<i+1;j++) {
+				System.out.print(k++ + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
--- Patterns/src/Pascal_Triangle.java
+++ Patterns/src/Pascal_Triangle.java
@@ -3,7 +3,23 @@ public class Pascal_Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n=5;
+		int spaces = n;
+		for (int i = 0; i < n; i++) {
+			for(int s=1;s<=spaces;s++) {
+				System.out.print(" ");
+			}
+			int num=1;
+			for(int j=0;j<=i;j++) {
+				System.out.print(num+" ");
+				num = num*(i-j)/(j+1);
+			}
+			spaces--;
+			System.out.println();
+			
+		}
+	}
 
 	}
 
-}
+
