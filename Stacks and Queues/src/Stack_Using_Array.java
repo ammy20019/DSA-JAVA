bacf694c97ab4ed1f02bb48545d7b911c58a4b9a
ammy20019 <amit.18mis7250@vitap.ac.in>
2022-02-11 13:30:55 +0530

    Added stacks using arrays

--- Stacks and Queues/src/Stack_Using_Array.java
+++ Stacks and Queues/src/Stack_Using_Array.java
@@ -1,78 +1,82 @@
 import java.util.*;
 import java.io.*;
 public class Stack_Using_Array {
-	int size;
-	int arr[];
-	int top;
-	
-	Stack_Using_Array(int size){
-		this.size = size;
-		this.arr = new int[size];
-		this.top = -1;
-	}
-	
-	public void push(int ele) {
-		if(!isFull()) {
-			top++;
-			arr[top] = ele;
-			System.out.println("Pushed element "+ ele);
-		} else {
-			System.out.print("Stack is full!");
-		}
-	}
-	
-	public int pop() {
-		if(!isEmpty()) {
-			int returnedTop= top;
-			top--;
-			System.out.println("Popped Element :" + arr[returnedTop]);
-			return arr[returnedTop];
-		} else {
-			System.out.println("Stack is empty");
-			return -1;
-		}
-	}
-	
-	public int peek() {
-		if(this.isEmpty())
-			return arr[top];
-			else {
-				System.out.println("Stack is empty!");
-				return -1;
-			}
-			
-		}
-	
-	public boolean isEmpty() {
-		return (top==-1);
-	}
-	
-	public boolean isFull() {
-		return (size-1 == top);
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		Scanner sc = new Scanner(System.in);
-//		int n= sc.nextInt();
-		
-		Stack_Using_Array StackCustom = new Stack_Using_Array(10);
-        StackCustom.pop();
-        System.out.println("=================");
-        StackCustom.push(10);
-        StackCustom.push(30);
-        StackCustom.push(50);
-        StackCustom.push(40);
-        System.out.println("=================");
-        StackCustom.pop();
-        StackCustom.pop();
-        StackCustom.pop();
-        System.out.println("=================");
-		
-		
-
-	}
+    int size;
+    int top;
+    int arr[];
+    Stack_Using_Array(int size){
+        this.size = size;
+        this.arr = new int[size];
+        this.top=-1;
+    }
+    
+    public void push(int element){
+        if(!isFull()){
+            top++;
+            arr[top]=element;
+            System.out.println("Element pushed "+ element);
+        } else{
+            System.out.println("Stack is Full!");
+        }
+    }
+    
+    public int pop(){
+        if(!isEmpty()){
+            int returnedTOP =top;
+            System.out.println("Element popped "+arr[top]);
+            top--;
+            return arr[returnedTOP];
+        } else{
+            System.out.println("Stack underflow!");
+            return -1;
+        }
+    }
+    
+    public int peek(){
+        if(!isEmpty()){
+            System.out.println("Element u r seeing right now "+arr[top]);
+            return arr[top];
+        } else{
+            System.out.println("No element in Stack!");
+            return -1;
+        }
+    }
+    
+    public void display(){
+        System.out.print("Elements from bottom -> top : ");
+        for(int i=0;i<=top;i++){
+            if(!isEmpty()){
+                System.out.print(arr[i]+" ");
+            }
+            
+        }
+        System.out.println();
+    }
+    
+    public boolean isEmpty(){
+        return (top==-1);
+    }
+    
+    public boolean isFull(){
+        return (size-1 == top);
+    }
+    
+    public static void main(String args[]) {
+        Stack_Using_Array ob = new Stack_Using_Array(3);
+        ob.push(10);
+        ob.push(90);
+        ob.push(20);
+        ob.pop();
+        ob.peek();
+        ob.push(50);
+        ob.display();
+        ob.push(293);
+        ob.pop();
+        ob.pop();
+        ob.display();
+        ob.pop();
+        ob.pop();
+      
+    }
 
 }
